65b2f76d98265958e4710ddc1ad72bcb48716eb9
Scott Wears <scottwears1@outlook.com>
2022-01-03 12:39:14 +0000

    add basic routing to the API

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const apiRouter = require('./routes/apiRoutes')
 
 const app = express();
 
 // for cross site calling
 app.use(cors());
 
+//A base route, every call to ip:port/api is first directed here
+app.use("/api", apiRouter)
 
 //A catch all route for any pages that dont exist
 app.all('*', (req, res) => {
