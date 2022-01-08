79cce803dd00f2b4f57cec1b09cbf009a1d3b2e8
Scott Wears <scottwears1@outlook.com>
2022-01-08 11:31:47 +0000

    change the port to Heroku's default 80

--- listen.js
+++ listen.js
@@ -1,7 +1,7 @@
 const app = require('./app.js');
 
 //A variable for the port number to make it configurable
-const port = 9000;
+const port = 80;
 
 //calls the app listener on the port number variable. when accessed returns "Hello World"
 app.listen(port, () => console.log(`running on port ${port}`));
